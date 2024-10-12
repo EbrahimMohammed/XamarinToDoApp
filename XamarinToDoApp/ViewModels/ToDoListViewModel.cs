3957cdffab5c4c95af118824abb016f993361f6e
EbrahimMohammed <haamaatya@gmail.com>
2024-10-12 23:24:37 +0300

    Added entry and bind it to NewToDoTextValue field

---
+++ XamarinToDoApp/ViewModels/ToDoListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using XamarinToDoApp.Models;
+
+namespace XamarinToDoApp.ViewModels
+{
+    public class ToDoListViewModel
+    {
+        public string NewToDoTextValue { get; set; }
+
+    }
+}
