e42dc2930207d76a21a46dc9dfcbeea5d426f7d5
EbrahimMohammed <haamaatya@gmail.com>
2024-10-12 23:15:34 +0300

    Create to do item model

---
+++ XamarinToDoApp/Models/ToDoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinToDoApp.Models
+{
+    public class ToDoItem
+    {
+        public string Text { get; set; }
+
+        public bool Complete { get; set; }
+       
+    }
+}
