f083463d6197947c3c1635bceae3959ca45c8282
EbrahimMohammed <haamaatya@gmail.com>
2024-10-13 22:13:34 +0300

    Implemented to do toggling

--- XamarinToDoApp/Models/ToDoItem.cs
+++ XamarinToDoApp/Models/ToDoItem.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace XamarinToDoApp.Models
@@ -8,12 +10,31 @@ namespace XamarinToDoApp.Models
     {
         public string Text { get; set; }
 
-        public bool Complete { get; set; }
+        public bool _Completed = false;
 
+        public bool Completed
+        {
+            get
+            {
+                return _Completed;
+            }
+            set
+            {
+                _Completed = value;
+                this.OnPropertyChanged("Completed");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
         public ToDoItem(string text, bool compelte)
         {
             Text = text;
-            Complete = compelte;
+            Completed = compelte;
         }
 
     }
